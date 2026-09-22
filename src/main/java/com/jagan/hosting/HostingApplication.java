469fb2b02c99152ca69f92f06795495c478269e8
Jagansureshkumar D <jaigardr@gmail.com>
2026-09-22 11:46:21 +0000

    Convert static website to Spring Boot application

---
+++ src/main/java/com/jagan/hosting/HostingApplication.java
@@ -0,0 +1,12 @@
+package com.jagan.hosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostingApplication.class, args);
+    }
+}
